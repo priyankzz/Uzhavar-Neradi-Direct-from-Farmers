c9e95e5bb377f01ae7d90b16a246b7070db3339b
Priyanka <oprpriyanka2024@gmail.com>
2026-03-30 21:41:27 +0530

    Switch database from SQLite to PostgreSQL

--- uzhavar-neradi/backend/core/settings.py
+++ uzhavar-neradi/backend/core/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DB', default='uzhavar'),
+        'USER': env('POSTGRES_USER', default='postgres'),
+        'PASSWORD': env('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': env('POSTGRES_HOST', default='db'),
+        'PORT': env('POSTGRES_PORT', default='5432'),
     }
 }
 
